af872905443b23030927143c61999841dacea86b
vivek saud <viveksaud3@gmail.com>
2023-06-02 15:41:50 +0545

    added environment variable

--- server/index.js
+++ server/index.js
@@ -2,6 +2,9 @@ import express from 'express';
 // import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv'
+
+dotenv.config();
 
 import postRoutes from './routes/posts.js'
 
@@ -17,10 +20,10 @@ app.use('/posts', postRoutes);
 
 
 
-const CONNECTION_URL = 'mongodb+srv://viveksaud_1:viveksaud_1password@cluster0.5soyxg6.mongodb.net/?retryWrites=true&w=majority'
+// const CONNECTION_URL = "mongodb+srv://viveksaud_1:viveksaud_1password@cluster0.5soyxg6.mongodb.net/?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch((error) => console.log(error.message));
 
